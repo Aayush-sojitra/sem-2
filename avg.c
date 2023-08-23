8c5ba82340be79f8f1e9673591f2292213c82ba7
Aayush-sojitra <142959829+Aayush-sojitra@users.noreply.github.com>
2023-08-23 08:56:48 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	void main(){
+		int a,b,c,avg;
+		printf("Enter a= ");
+		scanf("%d",&a);
+		printf("Enter b= ");
+		scanf("%d",&b);
+		printf("Enter c= ");
+		scanf("%d",&c);
+		avg=(a+b+c)/3;
+		printf("avg= %d",avg);
+	}
