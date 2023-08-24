6c5bedefb726bd00b24127dd0d32fd716b75d31b
Aayush-sojitra <142959829+Aayush-sojitra@users.noreply.github.com>
2023-08-24 12:16:20 +0530

    Add files via upload

---
+++ time conversation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	int sec,hour,min,time;
+	printf("enter sec:");
+	scanf("%d",&sec);
+	hour=sec/3600;
+	sec=sec%3600;
+	min=sec/60;
+	sec=sec%60;
+	printf("time=%d%d%d",hour,min,sec);
+	
+}
