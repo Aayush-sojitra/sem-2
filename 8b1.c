b81dbd3b717dd49e97fe0bd3419b5d77060e5698
Aayush sojitra <142959829+Aayush-sojitra@users.noreply.github.com>
2023-09-21 15:48:23 +0530

    Create 8b1.c

---
+++ 8b1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int i=0;
+    float s;
+    while(i<=9){
+        s=sqrt(i);
+        printf("num : %d ,sqrt : %f"          ,i,sqrt);
+        i=i+1;
+    }
+}
