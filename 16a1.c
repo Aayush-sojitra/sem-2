7e8f0ce4c370fc30f37ca1abb43fa6d1c1a673fa
Aayush sojitra <142959829+Aayush-sojitra@users.noreply.github.com>
2023-10-19 12:26:20 +0530

    Add files via upload

---
+++ 16a1.c
@@ -0,0 +1,17 @@
+//Read values in two-dimensional array and print them in matrix form.
+#include<stdio.h>
+void main(){
+	int a[2][2],i,j;
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("enter an element in a[%d][%d] :",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
